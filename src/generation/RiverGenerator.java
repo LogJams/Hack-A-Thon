36f62d65a02f0378adf57a3347cefebeee557dd2
IanMahuta <mahutai@msoe.edu>
2014-10-25 17:09:22 -0500

    Working on Rivers

--- src/generation/RiverGenerator.java
+++ src/generation/RiverGenerator.java
@@ -1,5 +1,38 @@
 package generation;
 
-public class RiverGenerator {
+import java.util.ArrayList;
 
+import main.Land;
+
+public class RiverGenerator {
+	public void generate(Land[][] world, float heightThreshold, float lakeLevel){
+		ArrayList<Land> startOptions = new ArrayList<Land>();
+		ArrayList<Land> riverStarts = new ArrayList<Land>();
+		for(int i = 0; i < world.length; i++){
+			for(int j = 0; j < world[0].length; j++){
+				if(world[i][j].height>=heightThreshold){
+					startOptions.add(world[i][j]);
+				}
+			}
+		}
+		
+		int numRivers = (int) Math.random()*startOptions.size();
+		int rand = 0;
+		for(int i = 0; i < numRivers; i++){
+			rand = (int) Math.random()*startOptions.size();
+			if(rand == startOptions.size()){
+				rand--;
+			}
+			riverStarts.add(startOptions.get(rand));
+			startOptions.remove(rand);
+		}
+		
+		for(int i = 0; i<riverStarts.size(); i++){
+			buildRiver(riverStarts.get(i));
+		}
+	}
+	
+	public void buildRiver(Land land){
+		land.isWet = true;
+	}
 }

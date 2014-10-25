d8740447089a6203e4a22ef8a53922cc8483f1cb
Logiant <beaverl@msoe.edi>
2014-10-25 11:11:20 -0500

    Added a camera class to maneuver in 3D space
    Woo!

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -4,12 +4,15 @@ public class Controller {
 
 	private Render draw;
 	WorldGenerator worldGen;
+	Camera cam;
 	
 	private int worldSize = 20;
+	public static final float landSpacing = 1.2f;
 	Land[][] world;
 	
 	public Controller() {
 		draw = new Render();
+		cam = new Camera();
 		worldGen = new WorldGenerator();
 		world = new Land[worldSize][worldSize];
 	}
@@ -19,6 +22,7 @@ public class Controller {
 		worldGen.update(world);
 		//render
 		draw.update(world);
+		cam.update();
 	}
 	
 }

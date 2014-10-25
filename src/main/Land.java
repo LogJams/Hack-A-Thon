c500fbf66fc2281c4687ce4bb0a686c2984218cc
IanMahuta <mahutai@msoe.edu>
2014-10-25 13:28:28 -0500

    I think generation works?
    Added height to Land and WorldGenarator functionality

--- src/main/Land.java
+++ src/main/Land.java
@@ -1,6 +1,14 @@
 package main;
 
 public class Land {
-
+	
+	// x coord?
+	// y coord?
+	
 	public float height;
+	public float waterLevel = 0;
+	
+	public Land(float height){
+		this.height = height;
+	}
 }
--- src/main/WorldGenerator.java
+++ src/main/WorldGenerator.java
@@ -2,9 +2,75 @@ package main;
 
 public class WorldGenerator {
 
+	//get in world and change the height
+	float xFreq = 5;
+	float zFreq = 8;
+	float[][] functionResults;
+	int prime1 = 15731;
+	int prime2 = 789221;
+	int prime3 = 1376312589;
+	Land[][] world;
+
+
 	public void update(Land[][] world) {
+		this.world = world;
+		functionResults = new float[(int) (world.length/xFreq)][(int) (world[0].length/zFreq)];
+		int x = 0;
+		int z = 0;
+		for(int i = 0; i<world.length/xFreq; i++){
+			for(int j = 0; j<world.length/zFreq; i++){
+				functionResults[i][j] = function(x,z);
+				z+=zFreq;
+			}
+			x+=xFreq;
+		}
 		
-		
-		
+		for(int i = 0; i < world.length; i++){
+			for(int j = 0; j<world.length; j++){
+				world[i][j].height = interpolate(i,j);
+			}
+		}
+	}
+
+	public float function(int x, int z){
+		x = (z^x) << (x)^z;
+		//x = (x<<13)^x;
+		return Math.abs((1.0f - ((x * (x * x * prime1 + prime2) + prime3) & 0x7fffffff) * 0.000000000931322574615478515625f));
+
+	}
+
+	public float interpolate(int x, int z){
+		int altx = x-2;
+		float[] interps = new float[4];
+		for(int i = 0; i<4; i++){
+			int left = (int) (altx/xFreq);
+			int right = left+1;
+
+			if(left == 0){
+				interps[i] = cubicInterpolate(functionResults[left][z], functionResults[left][z], functionResults[right][z], functionResults[right+1][z], (int)(x%xFreq));
+			}else if(right == world.length){
+				interps[i] = cubicInterpolate(functionResults[left-1][z], functionResults[left][z], functionResults[right][z], functionResults[right][z], (int)(x%xFreq));
+			}
+			else{
+				interps[i] = cubicInterpolate(functionResults[left-1][z], functionResults[left][z], functionResults[right][z], functionResults[right+1][z], (int)(x%xFreq));
+			}
+			altx++;
+			if(altx == x){
+				altx++;
+			}
+		}
+
+		float fullInterp = cubicInterpolate(interps[0], interps[1], interps[2], interps[3], x);
+
+		return fullInterp;
+	}
+
+	private float cubicInterpolate(float v0, float v1, float v2, float v3, float x) {
+		float P = (v3 - v2) - (v0 - v1);
+		float Q = (v0 - v1) - P;
+		float R = v2 - v0;
+		float S = v1;
+
+		return P*x*x*x + Q*x*x + R*x + S;
 	}
 }
